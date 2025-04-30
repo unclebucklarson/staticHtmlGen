3e12fee43b66c7a35c54bb6eaf3ebc15c44c8abe
Uncle Buck <unclebuck.larson@yahoo.com>
2025-04-30 13:39:31 -0500

    adding work on delimiter

---
+++ src/utilities.py
@@ -0,0 +1,32 @@
+from htmlnode import TextNode
+from htmlnode import HTMLNode
+from htmlnode import LeafNode
+from htmlnode import ParentNode
+from htmlnode import TextType
+from htmlnode import text_node_to_html_node
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    type_of_node = str()
+    new_nodes = list()
+    
+    if delimiter == "**":
+        type_of_node = TextType.BOLD
+        
+    elif delimeter ==  "`":
+        type_of_node = TextType.CODE
+    elif delimeter == "_":
+        type_of_delimeter = TextType.ITALIC
+    else:
+        typte_of_text = TextType.TEXT
+    
+    for node in old_nodes:
+        
+        if node.TextType != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        split_text = node.text.split(delimiter)
+        
+        
+    pass
+
